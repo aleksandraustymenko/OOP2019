0cdaa501dde90081c14af381d356690ab42d2ee7
aleksandraustymenko <46462352+aleksandraustymenko@users.noreply.github.com>
2019-10-18 14:37:54 +0200

    Add files via upload

---
+++ Lesson2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//4
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int netto,brutto;
+            int podatek;
+            Console.WriteLine("Podaj wartosc netto i podatek: ");
+            netto = Convert.ToInt32(Console.ReadLine());
+            podatek = Convert.ToInt32(Console.ReadLine());
+            brutto = netto*(1+podatek)/100;
+            Console.WriteLine("{0} netto  to {1} brutto", netto, brutto);
+
+            Console.ReadKey();
+        }
+    }
+}
